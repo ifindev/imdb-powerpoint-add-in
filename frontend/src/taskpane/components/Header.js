9a9d3784dc708ea04e864a7b1648e190baf58be9
muhammad.arifin <arifin.muhammad2610@Gmail.com>
2021-05-24 10:56:37 +0700

    change function name on Header.js

--- frontend/src/taskpane/components/Header.js
+++ frontend/src/taskpane/components/Header.js
@@ -1,17 +1,17 @@
 import React from "react";
 import useStyles from "./styles";
 
-const Header = ({ fetchMoviesHandle, resetMoviesHandle }) => {
+const Header = ({ fetchMovies, resetMovies }) => {
   const classes = useStyles();
   return (
     <div>
       <header className={classes.header}>
         <h1>Get Top IMDB Movies</h1>
         <div>
-          <button onClick={fetchMoviesHandle} className={`${classes.button} ${classes.buttonPrimary}`}>
+          <button onClick={fetchMovies} className={`${classes.button} ${classes.buttonPrimary}`}>
             Get Movie Lists
           </button>
-          <button onClick={resetMoviesHandle} className={`${classes.button} ${classes.buttonDanger}`}>
+          <button onClick={resetMovies} className={`${classes.button} ${classes.buttonDanger}`}>
             Clear Data
           </button>
         </div>
