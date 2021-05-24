7f962e31bea5709ddee052b9ea445c7f55c8d3d8
muhammad.arifin <arifin.muhammad2610@Gmail.com>
2021-05-24 10:58:28 +0700

    Add handleFetchMovies function and movies & loading state

--- frontend/src/taskpane/components/App.js
+++ frontend/src/taskpane/components/App.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
@@ -11,6 +11,21 @@ import useStyles from "./styles";
 
 export default function App() {
   const classes = useStyles();
+
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const handleFetchMovies = async () => {
+    setLoading(true);
+    setMovies([]);
+    const resp = await fetch("http://localhost:8080/imdb");
+    const imdbTopMovies = await resp.json();
+
+    setMovies(imdbTopMovies);
+    console.log(imdbTopMovies);
+    setLoading(false);
+  };
+
   return (
     <div className={classes.root}>
       <Header />
