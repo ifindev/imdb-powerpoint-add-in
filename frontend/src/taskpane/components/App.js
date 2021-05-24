4255dcdd4719948394a82f501ad4d63411e1b6e2
muhammad.arifin <arifin.muhammad2610@Gmail.com>
2021-05-24 10:41:36 +0700

    Add import on App.js

--- frontend/src/taskpane/components/App.js
+++ frontend/src/taskpane/components/App.js
@@ -1,5 +1,11 @@
 import * as React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import CircularProgress from "@material-ui/core/CircularProgress";
 /* global console, Office */
 
 export default function App() {}
